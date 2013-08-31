cfb8fb0dedd024ba67cfc1496ee599126d956888
Ronnie Nyaga <rnafundi@gmail.com>
2013-09-01 00:08:51 +0300

    Added an appropriate on a per-page basis.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,29 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def movies
+  	@title ="Movies"
   end
 
   def schedule
+  	@title = "Schedule"
   end
 
   def contacts
+  	@title = "Contacts"
   end
 
   def about
+  	@title = "About"
   end
 
   def help
+  	@title = "Help"
   end
 
   def contactus
+  	@title = "Contact Us"
   end
 end
